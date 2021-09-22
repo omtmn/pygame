84634dd8e6bc60e674fe4b332797275e02821efd
Alex <omtmn@icloud.com>
2021-09-21 23:34:57 -0400

    both ship movement, red ship unable to move back?

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ pygame.display.set_caption("My First Game")
 WHITE = (255, 255, 255)
 FPS = 60
 VELOCITY = 5
+BORDER = pygame.Rect(WIDTH/2 - 5, 0, 10, HEIGHT)
+BLACK = (0, 0, 0)
 
 
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(
@@ -23,30 +25,31 @@ RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
 
 def window_draw(red, yellow):
     WIN.fill(WHITE)
+    pygame.draw.rect(WIN, BLACK, BORDER)
     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))  # drawing a surface onto the screen (text or image)
     WIN.blit(RED_SPACESHIP, (red.x, red.y))
     pygame.display.update()
 
 
 def handle_yellow_movement(keys_pressed, yellow):
-    if keys_pressed[pygame.K_a]:  # (wasd keys)
+    if keys_pressed[pygame.K_a] and yellow.x - VELOCITY > 0:  # (wasd keys)
         yellow.x -= VELOCITY
-    if keys_pressed[pygame.K_d]:
+    if keys_pressed[pygame.K_d] and yellow.x + VELOCITY + yellow.width < BORDER.x:   # wont let us move past the border
         yellow.x += VELOCITY
-    if keys_pressed[pygame.K_w]:
+    if keys_pressed[pygame.K_w] and yellow.y - VELOCITY > 0:
         yellow.y -= VELOCITY
-    if keys_pressed[pygame.K_s]:
+    if keys_pressed[pygame.K_s] and yellow.y + VELOCITY + yellow.height < HEIGHT - 20:
         yellow.y += VELOCITY
 
 
 def handle_red_movement(keys_pressed, red):
-    if keys_pressed[pygame.K_LEFT]:  # (wasd keys)
+    if keys_pressed[pygame.K_LEFT] and red.x - VELOCITY > 0:  # (wasd keys)
         red.x -= VELOCITY
-    if keys_pressed[pygame.K_RIGHT]:
+    if keys_pressed[pygame.K_RIGHT] and red.x + VELOCITY + red.width < BORDER.x:
         red.x += VELOCITY
-    if keys_pressed[pygame.K_UP]:
+    if keys_pressed[pygame.K_UP] and red.y - VELOCITY > 0:
         red.y -= VELOCITY
-    if keys_pressed[pygame.K_DOWN]:
+    if keys_pressed[pygame.K_DOWN] and red.y + VELOCITY + red.height < HEIGHT - 20:
         red.y += VELOCITY
 
 
