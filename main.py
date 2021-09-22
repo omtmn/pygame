49be22f9255ec0486d7a31a73fd0b2ab03c4e50b
Alex <omtmn@icloud.com>
2021-09-21 23:08:21 -0400

    spaceships displayed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import os   # importing os to handle paths for assets
 
 pygame.init()
 
@@ -6,13 +7,29 @@ WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My First Game")
 WHITE = (255, 255, 255)
-FPS = 30
+FPS = 60
 
-def window_draw():
+
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join('Assets', 'spaceship_yellow.png'))
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
+    YELLOW_SPACESHIP_IMAGE, (55, 40)), 90)   # resizing image
+RED_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join('Assets', 'spaceship_red.png'))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
+    RED_SPACESHIP_IMAGE, (55, 40)), 270)
+
+
+def window_draw(red, yellow):
     WIN.fill(WHITE)
+    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))  # drawing a surface onto the screen (text or image)
+    WIN.blit(RED_SPACESHIP, (red.x, red.y))
     pygame.display.update()
 
+
 def main():
+    red = pygame.Rect(700, 300, 55, 40)
+    yellow = pygame.Rect(100, 300, 55, 40)
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -20,8 +37,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        window_draw()
+
+        window_draw(red, yellow)
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
